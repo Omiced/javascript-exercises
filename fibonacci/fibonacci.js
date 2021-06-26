e8cae5d8d2fc1974a8c402454f0fbb51c155e804
Josue Cano <jdevelopcano@gmail.com>
2021-06-25 21:14:10 -0500

    Terminado Fibbonaci

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function (n) {
+	let num = Number(n);
+	if (num < 0) {
+		return "OOPS";
+	}
+	else if (num <= 1) {
+		return num;
+	}
+	return fibonacci(num - 1) + fibonacci(num - 2);
 };
 
 module.exports = fibonacci;
