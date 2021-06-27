6bf7bd46cb7027a16ea03329bcbb3ba9de0a9315
Josue Cano <jdevelopcano@gmail.com>
2021-06-27 17:58:32 -0500

    terminado palindromos y findTheOldest

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+	const wordWithoutSigns = word.replace(/[!,.]/g, "");
+	const wordClean = wordWithoutSigns.replace(/ /g, "").toLowerCase();
+	let wordReverse = "";
+	for (x = wordClean.length - 1; x >= 0; x--) {
+		wordReverse += wordClean[x];
+	}
+	return wordReverse === wordClean;
 };
 
 module.exports = palindromes;
